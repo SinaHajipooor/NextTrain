7bd843dabaa975d52fac75aa49e50c48736dca0e
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-10 19:03:53 +0330

    remove test files

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,9 @@
-import Link from "next/link"
-
 function Home() {
 
 
     return (
         <div>
-            <br />
-            <br />
-            <Link href='/serverSide'>server page</Link>
-            <br />
-            <br />
-            <Link href='/clientSide'>client page</Link>
+
         </div>
     )
 }
