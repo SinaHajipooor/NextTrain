0e3c1f128a3dc703c3d438c5b0982d7d8bfac986
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-10 19:50:30 +0330

    add 404 custom page

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+    return (
+        <div>
+            <h1>
+                we couldnt find the page you want
+            </h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router"
+
 function ClientsProjectPage() {
+    const router = useRouter()
     return (
         <div>
-            the projects of a given client
+            <h1>the projects of a given client</h1>
+            <br />
+            <br />
+            <button onClick={() => router.push('/clients/max/a ')}>Load project A</button>
         </div>
     )
 }
