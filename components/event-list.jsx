9903af10c443e4cd53fc7c5ed373a7115559ac07
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-10 20:34:49 +0330

    display events lict at root

--- components/event-list.jsx
+++ components/event-list.jsx
@@ -3,7 +3,7 @@ import EventItem from "./event-item"
 function EventsList({ items }) {
     return (
         <ul>
-            {items.map(item => <EventItem key={item.id} />)}
+            {items.map(item => <EventItem key={item.id} event={item} />)}
         </ul>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
+import EventsList from "../components/event-list";
 import { getFeaturedEvents } from "../dummy-data"
 
 function HomePage() {
     // get all events
     const featuredEvents = getFeaturedEvents();
 
+    //ui
     return (
         <div>
-            <h1>The Home page</h1>
+            <EventsList items={featuredEvents} />
         </div>
     )
 }
