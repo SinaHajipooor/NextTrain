34b85dcee83638c6541b528293d6c9c2039d5906
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-11 12:53:54 +0330

    add server side rendering for activity detail

--- pages/[aid].jsx
+++ pages/[aid].jsx
@@ -1,5 +1,4 @@
 import { apiShowActivityHistory } from "../api/apiActivityHistory";
-import axiosConfig from "../utils/axios";
 
 function ActivityDetail({ activityHistory }) {
 
@@ -16,12 +15,48 @@ function ActivityDetail({ activityHistory }) {
 };
 
 
-export async function getStaticProps(context) {
-    // params is an object full of key-value  
+export default ActivityDetail
+
+
+// ----------- approach 1 ----------------
+
+// export async function getStaticProps(context) {
+//     // params is an object full of key-value
+//     const { params } = context;
+//     const activityId = params.aid;
+
+//     const data = await apiShowActivityHistory(activityId)
+//     return {
+//         props: {
+//             activityHistory: data
+//         }
+//     }
+// }
+
+// // to tel next.js which instances of this dynamic page should be generated
+// export async function getStaticPaths() {
+
+//     const response = await axiosConfig.get('/api/profile/activity-history?user_id=1');
+//     const data = response.data.result.data;
+
+//     const ids = data.map(activity => activity.id);
+//     const pathsWithParams = ids.map(id => ({ params: { aid: id.toString() } }))
+
+//     return {
+//         paths: pathsWithParams,
+//         fallback: false,
+//         // fallback: 'blocking', // when we have a lot pages , in this case we dont have to wory about excepting null immediestly because it will wait for data to be fetched .
+//         //     fallback : true  , in this situation we tell next that even the pages which are not listed in paths , can be valid pages that should be genarated but just when they are visited but they shouldnt pre-generated instaed   ( note : when we are using fallback set to true we should except null values because the data doesnt come immedietly)
+//     }
+// }
+
+// ----------- approach 2 ----------------
+export async function getServerSideProps(context) {
     const { params } = context;
     const activityId = params.aid;
 
-    const data = await apiShowActivityHistory(activityId)
+    const data = await apiShowActivityHistory(activityId);
+
     return {
         props: {
             activityHistory: data
@@ -29,24 +64,4 @@ export async function getStaticProps(context) {
     }
 }
 
-// to tel next.js which instances of this dynamic page should be generated
-export async function getStaticPaths() {
 
-    const response = await axiosConfig.get('/api/profile/activity-history?user_id=1');
-    const data = response.data.result.data;
-
-    const ids = data.map(activity => activity.id);
-    const pathsWithParams = ids.map(id => ({ params: { aid: id.toString() } }))
-
-    return {
-        paths: pathsWithParams,
-        fallback: false,
-        // fallback: 'blocking', // when we have a lot pages , in this case we dont have to wory about excepting null immediestly because it will wait for data to be fetched . 
-        //     fallback : true  , in this situation we tell next that even the pages which are not listed in paths , can be valid pages that should be genarated but just when they are visited but they shouldnt pre-generated instaed   ( note : when we are using fallback set to true we should except null values because the data doesnt come immedietly)
-    }
-}
-
-
-
-
-export default ActivityDetail
