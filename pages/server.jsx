650aa7b59189c30509785eae0c4da70948975851
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-11 20:15:37 +0330

    add server page head

--- pages/server.jsx
+++ pages/server.jsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import { apiFetchAllActivityHistories } from "../api/apiActivityHistory";
+import Head from "next/head";
 
 function ServerSide(props) {
     return (
         <div style={{ marginLeft: '50px', marginTop: '50px' }}>
+            <Head>
+                <title>
+                    server
+                </title>
+            </Head>
             {props.activityHistories.map(activity => <li key={activity.id}><Link href={`/${activity.id}`} >{activity.title}</Link> </li>)}
         </div>
     )
