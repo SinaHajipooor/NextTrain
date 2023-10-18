78d832bfb1ee7c95c80e9c146560f4740e42431c
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-18 09:07:24 +0330

    optimize

--- api/apiActivityHistory.js
+++ api/apiActivityHistory.js
@@ -1,24 +1,24 @@
 import axiosConfig from "../utils/axios";
 
 export async function apiShowActivityHistory(id) {
-    try {
-        const response = await axiosConfig.get(`/api/profile/activity-history/show/${id}?user_id=1`);
-        const data = response.data.result;
+    //     try {
+    const response = await axiosConfig.get(`/api/profile/activity-history/show/${id}?user_id=1`);
+    const data = response.data.result;
 
-        return data;
-    } catch (error) {
-        // throw new Error(error.message)
-    }
+    return data;
+    //     } catch (error) {
+    // throw new Error(error.message)
+    //     }
 }
 
 
 export async function apiFetchAllActivityHistories() {
-    try {
-        const response = await axiosConfig.get(`/api/profile/activity-history?user_id=1`);
-        const data = response.data.result.data;
+    //     try {
+    const response = await axiosConfig.get(`/api/profile/activity-history?user_id=1`);
+    const data = response.data.result.data;
 
-        return data;
-    } catch (error) {
-        // throw new Error(error.message)
-    }
+    return data;
+    //     } catch (error) {
+    // throw new Error(error.message)
+    //     }
 }
